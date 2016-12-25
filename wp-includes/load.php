69c1e28ff3606647d78cf241e548b72bf7952d75
zqh2016 <408868908@qq.com>
2016-12-25 12:53:44 +0800

    s

--- wp-includes/load.php
+++ wp-includes/load.php
@@ -297,7 +297,8 @@ function wp_debug_mode() {
 			ini_set( 'error_log', WP_CONTENT_DIR . '/debug.log' );
 		}
 	} else {
-		error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+		//error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR | E_WARNING | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
+		error_reporting( E_CORE_ERROR | E_CORE_WARNING | E_COMPILE_ERROR | E_ERROR  | E_PARSE | E_USER_ERROR | E_USER_WARNING | E_RECOVERABLE_ERROR );
 	}
 	if ( defined( 'XMLRPC_REQUEST' ) )
 		ini_set( 'display_errors', 0 );
